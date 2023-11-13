686cd793f9704e7c8f558e04f6ae0deadff70825
DarrylBD99 <darrylhoBD@gmail.com>
2023-11-13 22:30:24 +0800

    Update NPC.gd

--- Scripts/Entity/NPC/NPC.gd
+++ Scripts/Entity/NPC/NPC.gd
@@ -1,14 +1,12 @@
 extends Entity
 class_name NPC
 
-@export var target : Entity
-
 var target_direction : Vector2
 var target_distance : float
 	
 func _physics_process(delta):
-	if ( weakref(target)) :
-		var targetPosition = target.global_position
+	if ( weakref(GameManager.target)) :
+		var targetPosition = GameManager.target.global_position
 		var current_agent_position = global_position	
 		var difference = targetPosition - current_agent_position
 		target_direction = difference.normalized()
