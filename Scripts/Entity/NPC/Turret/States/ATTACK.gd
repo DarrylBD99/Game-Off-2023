6ebc92db2604639e26bf141a7586dc5c792c7100
DarrylBD99 <darrylhoBD@gmail.com>
2023-11-14 12:36:25 +0800

    added sfx to turret

--- Scripts/Entity/NPC/Turret/States/ATTACK.gd
+++ Scripts/Entity/NPC/Turret/States/ATTACK.gd
@@ -6,6 +6,16 @@ extends State
 var range_bool : bool
 var block_bool : bool
 
+var audio_stream_player : AudioStreamPlayer2D
+
+func state_ready():
+	var attack_1_audio : AudioStream = preload("res://Audio/SE/Attack1.wav")
+	audio_stream_player = AudioStreamPlayer2D.new()
+	
+	audio_stream_player.set_stream(attack_1_audio)
+	audio_stream_player.set_bus("SFX")
+	entity.add_child(audio_stream_player)
+
 func is_ready() -> bool:
 	range_bool = entity.target_distance < target_range
 	block_bool = entity.AimRayCast.is_colliding()
@@ -29,6 +39,8 @@ func physics_process(delta : float) -> State:
 	
 	if entity.can_fire:
 		entity.shoot_bullet(entity.bullet_scene, entity.barrel_end.global_position, GameManager.target.global_position)
+		
+		audio_stream_player.play()
 	
 	if not (range_bool and not block_bool):
 		return idle
