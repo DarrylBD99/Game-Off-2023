f63a74877225f9ab1caed07428a7843be7e9b60a
DarrylBD99 <darrylhoBD@gmail.com>
2023-11-19 17:42:24 +0800

    rolled back navigation agent

--- Scripts/Entity/NPC/NPC.gd
+++ Scripts/Entity/NPC/NPC.gd
@@ -2,8 +2,8 @@ extends Entity
 class_name NPC
 
 @export var aggro_range : float = 300
-#@export var debug_path = false
-@export var navigation_agent : NavigationAgent2D
+@export var debug_path = true
+#@export var navigation_agent : NavigationAgent2D
 
 var target: Entity
 
@@ -11,24 +11,24 @@ var target_direction : Vector2
 var target_distance : float
 
 var current_path: PackedVector2Array
-#var navigation_position : Vector2
+var navigation_position : Vector2
 var navigation_direction : Vector2
 
-#func _draw():
-#	if debug_path:
-#		for index in range(current_path.size() - 1):
-#			draw_line(
-#				current_path[index] - global_position, 
-#				current_path[index + 1 ]- global_position, 
-#				Color.RED, 
-#				1)
+func _draw():
+	if debug_path:
+		for index in range(current_path.size() - 1):
+			draw_line(
+				current_path[index] - global_position, 
+				current_path[index + 1 ]- global_position, 
+				Color.RED, 
+				1)
 					
 func _physics_process(delta):
 	update_sensor()
 	update_target()
 	update_navigation()
 	
-#	queue_redraw()
+	queue_redraw()
 	super._physics_process(delta)
 
 
@@ -41,7 +41,7 @@ func update_sensor():
 
 func update_target():
 	if target:
-		navigation_agent.target_position = target.global_position
+#		navigation_agent.target_position = target.global_position
 		
 		var target_position = target.global_position
 		var difference = target_position - global_position
@@ -49,17 +49,15 @@ func update_target():
 		target_distance = difference.length()
 
 func update_navigation():
-	if target:
-		navigation_direction = to_local(navigation_agent.get_next_path_position()).normalized()
-	
-#	if GameManager.grid_navmesh && target:
-#		var path  = GameManager.grid_navmesh.generate_path(
-#			global_position, 
-#			target.global_position)
-#				
-#		if (path.size() > 0):
-#			current_path = path
-#			navigation_position = current_path[1]
-#			navigation_direction = (navigation_position - global_position).normalized()
-	
-	
+#	if target:
+#		navigation_direction = to_local(navigation_agent.get_next_path_position()).normalized
+	if GameManager.grid_navmesh && target:
+		var path  = GameManager.grid_navmesh.generate_path(
+			global_position, 
+			target.global_position)
+		
+		if (path.size() > 1):
+			current_path = path
+			navigation_position = current_path[1]
+			navigation_direction = (navigation_position - global_position).normalized()
+
