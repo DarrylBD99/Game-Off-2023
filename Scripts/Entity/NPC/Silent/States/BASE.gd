73c16c22524995c37e4ceb09b0eea291236594d0
DarrylBD99 <darrylhoBD@gmail.com>
2023-11-24 21:29:11 +0800

    reworked silent

--- Scripts/Entity/NPC/Silent/States/BASE.gd
+++ Scripts/Entity/NPC/Silent/States/BASE.gd
@@ -5,10 +5,10 @@ extends State
 @export var health_bar : TextureProgressBar
 
 func physics_process(_delta : float) -> State:
-	if health_bar.modulate.a == 0:
-		return invis
-	
 	if (entity.target && entity.target_distance <= invis.attack_range):
 		return attack
+	
+	if health_bar.modulate.a == 0:
+		return invis
 	
 	return null
