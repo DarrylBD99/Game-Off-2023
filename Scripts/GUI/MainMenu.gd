0eccb394160bfec33f3d541d630522cc46ddd8e7
DarrylBD99 <darrylhoBD@gmail.com>
2023-11-29 18:37:17 +0800

    updated main menu

--- Scripts/GUI/MainMenu.gd
+++ Scripts/GUI/MainMenu.gd
@@ -5,22 +5,49 @@ extends Control
 @export var animation_player : AnimationPlayer
 @export var quit_button : Button
 
+var credit_roll : bool = false
+var hovering : bool = false
+
+var buttons : Array[Button]
+
 func _ready():
+	GameManager.set_default()
+	
 	if play_button:
+		buttons.append(play_button)
 		play_button.pressed.connect(play_game)
 	
 	if credit_button:
+		buttons.append(credit_button)
 		credit_button.pressed.connect(open_credits)
 	
 	if quit_button:
+		buttons.append(quit_button)
 		quit_button.pressed.connect(quit_game)
 
+func _process(_delta):
+	for button in buttons:
+		if button.is_hovered():
+			if not hovering:
+				$Hover.play()
+				hovering = true
+				return
+			else:
+				return
+	
+	hovering = false
+
 func play_game():
-	get_tree().change_scene_to_file("res://Scenes/Levels/LevelTest.tscn")
+	$Select.play()
+	Transition.change_scene("res://Scenes/Levels/LevelTest.tscn")
 
 func open_credits():
-	animation_player.play("show_credits")
+	$Select.play()
+	credit_roll = true
+	animation_player.play("credits")
+	await animation_player.animation_finished
 
 func quit_game():
-	get_tree().quit()
+	$Select.play()
+	Transition.quit()
 
