22b0197b0b637aabef521cf09c585a59789690ab
DarrylBD99 <darrylhoBD@gmail.com>
2023-11-29 18:37:48 +0800

    updated splash

--- Scripts/GUI/SplashScreen.gd
+++ Scripts/GUI/SplashScreen.gd
@@ -6,11 +6,9 @@ var preffered_size : Vector2i = Vector2i(1920, 1080)
 func _ready():
 	old_size = get_viewport().get_visible_rect().size
 	get_viewport().set_content_scale_size(preffered_size)
-	print(get_viewport().get_visible_rect().size)
 
 func open_main_menu():
 	ResourceLoader.load_threaded_request("res://Scenes/GUI/MainMenu.tscn")
 	get_viewport().set_content_scale_size(old_size)
-	print(get_viewport().get_visible_rect().size)
-	get_tree().change_scene_to_packed(ResourceLoader.load_threaded_get("res://Scenes/GUI/MainMenu.tscn"))
+	Transition.change_scene_packed(ResourceLoader.load_threaded_get("res://Scenes/GUI/MainMenu.tscn"))
 	
