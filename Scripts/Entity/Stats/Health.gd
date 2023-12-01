6101310afc74108d0f1178dde6eae9c7f0e87452
DarrylBD99 <darrylhoBD@gmail.com>
2023-12-02 02:42:33 +0800

    added passive healing

--- Scripts/Entity/Stats/Health.gd
+++ Scripts/Entity/Stats/Health.gd
@@ -4,6 +4,7 @@ class_name Health
 
 @export var hp_max : float = 10.0
 @export var shake_screen : bool = false
+@export var passive_rate : float = 0
 
 @onready var hp : float = hp_max
 
@@ -17,5 +18,11 @@ func damage(attack : Attack):
 	
 
 func _process(_delta):
+	hp += passive_rate
+	
+	if hp > hp_max:
+		hp = hp_max
+	
 	if hp <= 0:
+		get_parent().add_sibling(preload("res://Scenes/Particles/AcidBurst.tscn").instantiate())
 		get_parent().queue_free()
